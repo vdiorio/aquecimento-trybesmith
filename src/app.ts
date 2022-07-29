e405da9909580e312abef1dc61d8dc6a5792ccbd
vdiorio <vitor.diorio@yahoo.com.br>
2022-07-29 07:36:11 -0300

    :sparkles: Implementa router e middleware de erro na aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/error';
+import router from './router'
 
 const app = express();
 
 app.use(express.json());
+app.use(router)
+
+app.use(errorMiddleware)
 
 export default app;
